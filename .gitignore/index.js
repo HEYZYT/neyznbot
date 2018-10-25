17659bb30834f83400d25e54658c5f7ea945b7e0
HEYZYT <43936414+HEYZYT@users.noreply.github.com>
2018-10-25 22:25:40 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -72,7 +72,7 @@ bot.on('message', message => {
             }else{
                 return message.reply("Tu n'as pas la permission!")
         }
-   */ }
+    }*/
 })
 
 bot.on('message', message => {
